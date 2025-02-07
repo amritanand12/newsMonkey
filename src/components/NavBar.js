5d4fe454ab1b84fdbe76684d1140c191db612efd
AMRIT ANAND <61642500+amritanand12@users.noreply.github.com>
2025-02-07 11:57:35 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,7 +12,7 @@ const x = test();
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-
+div
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto active">
                             <li className="nav-item">
